9218c889757c0efd503bdc5afe0e7f6bee2d3465
nnarciss98 <nnarciss98@gmail.com>
2024-09-13 17:05:50 +0200

    First commit

---
+++ EldadApp/src/main/java/com/eldad/EldadApp/EldadAppApplication.java
@@ -0,0 +1,13 @@
+package com.eldad.EldadApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EldadAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EldadAppApplication.class, args);
+	}
+
+}
